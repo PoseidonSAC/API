707ce1aa7efdafc6bf8317b29b614a11b1b6f1f9
JosephRamirez-F4fu <U20211C828@upc.edu.pe>
2024-12-15 11:03:15 -0500

    Actualizar .gitignore para incluir el archivo .env y eliminar la verificación de SECRET_HASH en la configuración de JWT

--- src/core/constants/secret_jwt.ts
+++ src/core/constants/secret_jwt.ts
@@ -8,12 +8,8 @@ if (!process.env.PORT) {
 if (!process.env.SECRET_JWT) {
   throw new Error("JWT_SECRET must be defined");
 }
-if (!process.env.SECRET_HASH) {
-  throw new Error("SECRET_HASH must be defined");
-}
 
 export const ENV = {
   PORT: process.env.PORT,
   SECRET_JWT: process.env.SECRET_JWT,
-  SECRET_HASH: process.env.SECRET_HASH,
 };
